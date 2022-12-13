615e23656bcaa6a03e6be4dfb2332350d82bd88d
Wildan M <willnode@wellosoft.net>
2022-12-13 11:54:57 +0700

    Error fix bubble

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -1,15 +1,6 @@
-<style>
-    .editor {
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        padding: 8px;
-        min-height: 200px;
-        white-space: pre-wrap;
-    }
-</style>
-
 <script>
     import { onMount, onDestroy } from "svelte";
+    import { FixerElement } from "./FixerElement";
     import { restoreSelection, saveSelection } from "./selection";
 
     let lastUpdate = Date.now();
@@ -20,6 +11,9 @@
     let updateBeacon = null;
 
     onMount(() => {
+        /**
+         * @type {HTMLDivElement}
+         */
         const editor = document.querySelector(".editor");
         if (sessionStorage.getItem("text")) {
             editor.innerHTML = sessionStorage.getItem("text");
@@ -28,20 +22,35 @@
             lastUpdate = Date.now();
             if (curVersion !== lastVersion && Date.now() - curTime > 1000) {
                 lastVersion = curVersion;
-                editor.designMode = "off";
-                sessionStorage.setItem("text", editor.innerHTML);
-                fetch("https://api.benerin.co/v1/fix?word="+encodeURIComponent(curText), {
-                    method: "GET",
-                }).then((res) => {
-                    return res.json();
-                }).then((data) => {
-                    var sel = saveSelection(editor);
-                    editor.innerHTML = data.data;
-                    setTimeout(() => {
-                        restoreSelection(editor, sel);
-                    editor.designMode = "on";
-                    }, 100);
-                });
+                fetch(
+                    "https://api.benerin.co/v1/check?text=" +
+                        encodeURIComponent(curText),
+                    {
+                        method: "GET",
+                    }
+                )
+                    .then((res) => {
+                        return res.json();
+                    })
+                    .then((data) => {
+                        var sel = saveSelection(editor);
+                        editor.innerHTML = data.rendered;
+                        // init all fixer elements
+                        const fixerElements = document.querySelectorAll("u-x");
+                        fixerElements.forEach((el) => {
+                            var newItem = new FixerElement();
+                            newItem.setAttribute("data-type", el.getAttribute("data-type"));
+                            newItem.setAttribute("data-suggestion", el.getAttribute("data-suggestion"));
+                            newItem.innerText = el.innerText;
+                            el.parentNode.replaceChild(newItem, el);
+                        });
+
+                        setTimeout(() => {
+                            editor.focus();
+                            restoreSelection(editor, sel);
+                            sessionStorage.setItem("text", editor.innerHTML);
+                        }, 10);
+                    });
             }
         }, 1000);
         editor.addEventListener("input", (e) => {
@@ -55,9 +64,8 @@
         clearInterval(updateBeacon);
     });
 </script>
-<div class="editor" contenteditable>
 
-</div>
+<div class="editor" contenteditable />
 
 <div>
     {#if curVersion !== lastVersion}
@@ -66,3 +74,34 @@
         </div>
     {/if}
 </div>
+
+<style>
+    .editor {
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 8px;
+        min-height: 200px;
+        white-space: pre-wrap;
+    }
+
+    .editor :global(u-x) {
+        text-decoration: underline;
+        color: lightcoral;
+        cursor: pointer;
+    }
+
+    :global(.u-x-tooltip) {
+        display: none;
+        padding: 0.5em 1em;
+    }
+
+    :global(.u-x-tooltip) :global(b) {
+        background: rgb(8, 8, 8);
+        white-space: pre;
+    }
+    :global(.u-x-tooltip.show) {
+        display: block;
+        background: rgb(45, 45, 45);
+        color: rgb(255, 255, 255);
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import './app.css'
 import App from './App.svelte'
+import { FixerElement } from './lib/FixerElement';
 
 const app = new App({
   target: document.getElementById('app'),
 })
+
+if (window.customElements) {
+  customElements.define("u-x", FixerElement);
+}
 
 export default app
